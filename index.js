89b975526fd05c4371c105ef4bd7e9e1e9fa183e
Oli <oliver@digi.me>
2021-06-16 17:37:06 +0100

    change to accept obj, and use applicationId for external

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ import {getSessionURL, getPrivateShareGuestURL} from './src/sdk/urlPaths';
 import {authorise} from './src/sdk/authorise';
 import {request} from './src/sdk/request';
 
-const authOngoingOnce = async (applicationId, contractId, privateKey) => {
-    return await authorise(applicationId, contractId, privateKey);
+const authOngoingOnce = async ({applicationId: appId, contractId, privateKey}) => {
+    return await authorise(appId, contractId, privateKey);
 }
 
-const establishSession = async (appId, contractId) => {
+const establishSession = async ({applicationId: appId, contractId}) => {
     return await request.func.post(getSessionURL, {appId, contractId});
 }
 
-const guestShareURL = (sessionKey, callbackUrl) => {
+const guestShareURL = ({sessionKey, callbackUrl}) => {
     const baseurl = "https://api.digi.me"; // temp
     return getPrivateShareGuestURL(baseurl, sessionKey, callbackUrl)
 }
