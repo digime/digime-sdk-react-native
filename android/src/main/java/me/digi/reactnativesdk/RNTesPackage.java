fa436c1617bf492be1b35bbdd0cdbb19b97fd6e3
Oli <oliver@digi.me>
2021-09-06 13:17:47 +0100

    Fix RandomBytes package to create module using exposed functions

--- android/src/main/java/me/digi/reactnativesdk/RNTesPackage.java
+++ android/src/main/java/me/digi/reactnativesdk/RNTesPackage.java
@@ -1,6 +1,7 @@
-
 package me.digi.reactnativesdk;
 
+import java.lang.annotation.Native;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -11,17 +12,29 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
-// not required to hook up
-//import com.bitgo.randombytes.RandomBytesModule;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.proyecto26.inappbrowser.RNInAppBrowserModule;
 
 public class RNTesPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(
-        //new RandomBytesModule(reactContext),
-        new RNInAppBrowserModule(reactContext)
-      );
+        // Add RandomBytes native modules - the RandomBytesModule isn't a public class
+        // so we have to add via the public package first, which returns
+        // an array of modules, essentially one, that we need to add
+        RandomBytesPackage rnPackage = new RandomBytesPackage();
+        List<NativeModule> rnModules = rnPackage.createNativeModules(reactContext);
+
+        // Add other modules that are required
+        // here we add the InAppBrowser module
+        List<NativeModule> modules = new ArrayList<NativeModule>();
+        modules.add(new RNInAppBrowserModule(reactContext));
+
+        // combine lists
+        List<NativeModule> allModules = new ArrayList<NativeModule>();
+        allModules.addAll(rnModules);
+        allModules.addAll(modules);
+
+      return allModules;
     }
 
     // Deprecated from RN 0.47
